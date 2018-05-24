ebf33889846b787e02c729837d83e37a116df377
AlexLaak <alexla@metropolia.fi>
2018-05-24 17:32:56 +0300

    current v

--- python/Discord.py
+++ python/Discord.py
@@ -16,11 +16,16 @@ steamIDremoval = 76561197960265728
 @client.event
 async def on_ready():
     print("Bot is online and connected to Discord") #This will be called when the bot connects to the server
-
+    await client.change_presence(game=discord.Game(name='!commands'))
 @client.event
 async def on_message(message):
 
+    channel = message.channel.name
+    if (channel != "test"):
+        return
+
     try:
+
         namesAndIDs = pickle.load(open("save.p", "rb"))
     except (OSError, IOError):
         namesAndIDs = []
@@ -69,8 +74,12 @@ async def on_message(message):
         if (len(DotaID) <= 2 or DotaID[0] == ""):
             await client.send_message(message.channel, "Invalid ID!")
         else:
-            whitelistIDs.append(DotaID)
-        pickle.dump(whitelistIDs, open("whitelistt.p", "wb"))
+            if (whitelistIDs.__contains__(DotaID) == False):
+                    whitelistIDs.append(DotaID)
+                    pickle.dump(whitelistIDs, open("whitelistt.p", "wb"))
+
+    if (message.content.upper() == "!PRIZE" ):
+        await client.send_message(message.channel, await checkPricePool())
 
     if (message.content.upper() == "!LIST"):
         try:
@@ -89,6 +98,17 @@ async def on_message(message):
     if (message.content.upper() == "!JAFAR"):
         voice = await client.join_voice_channel(message.author.voice_channel)
         player = voice.create_ffmpeg_player('./sounds/jafar.mp3')
+        player.start()
+        counter = 0
+        duration = 4  # In seconds
+        while not counter >= duration:
+            await asyncio.sleep(1)
+            counter = counter + 1
+        await voice.disconnect()
+
+    if (message.content.upper() == "!ENIGMA"):
+        voice = await client.join_voice_channel(message.author.voice_channel)
+        player = voice.create_ffmpeg_player('./sounds/enigma.mp3')
         player.start()
         counter = 0
         duration = 4  # In seconds
@@ -145,7 +165,7 @@ async def on_message(message):
                 print(DotaID)
             await queryProfileNoAuth(message, client, DotaID)
 
-    if (message.content.upper() == "!LASTMATCH"):
+    if (message.content.upper() == "!LASTMATCHPLAYERS"):
         await queryLastMatch(message.author.id, message, client)
 
     if (message.content.upper().startswith("!PW") and message.author.id in namesAndIDs):
@@ -166,26 +186,38 @@ async def on_message(message):
                 print(DotaID)
             await playedWith(message.author.id,message,client,DotaID)
 
+    if (message.content.upper() == "!LASTMATCH"):
+        ID = await getProfileID(message.author.id)
+        if (ID == "None"):
+            await client.send_message(message.channel, "No ID set!")
+            return
+        await playedWithLastGame(await queryHelper(message.author.id),ID, message, client)
+
     if (message.content.upper() == "!COMMANDS" or message.content.upper() == "!HELP"):
         print("Comamnd was given by:")
         print(message.author)
-        await client.send_message(message.channel, "List of commands: \n```!add_id <dotaID>"
+        await client.send_message(message.channel, "List of commands:"
+                                                   "\n```!add_id <dotaID>"              
                                                    "\n!delete_id"
                                                    "\n!list"
                                                    "\n!me"
-                                                   "\n!pw <dotaID or steam64>"
+                                                   "\n!pw <dotaID/steam64>"
                                                    "\n!sounds"
-                                                   "\n!profile<dotaID or steam64```"
-                                                   "\n!lastmatch```")
+                                                   "\n!profile <dotaID/steam64>"
+                                                   "\n!lastmatch"
+                                                   "\n!lastmatchplayers"   
+                                                   "\n!prize```")
 
     if (message.content.upper() == "!SOUNDS"):
         print("Comamnd was given by:")
         print(message.author)
-        await client.send_message(message.channel, "List of sounds: \n```!joni"
-                                                   "\n!joni2```"
-                                                   "\n!jafar```")
+        await client.send_message(message.channel, "List of sounds:"
+                                                   "\n```!joni"
+                                                   "\n!joni2"
+                                                   "\n!jafar"
+                                                   "\n!enigma```")
 
-    if (message.content.upper() == "EXIT"):
+    if (message.content.upper() == "EXIT" and message.author.id == '95497315078381568'):
         print("Comamnd was given by:")
         print(message.author)
         pickle.dump(namesAndIDs,open("save.p","wb"))
--- python/methods.py
+++ python/methods.py
@@ -3,11 +3,12 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
 import requests
+from html.parser import HTMLParser
 import pickle
 from decimal import Decimal
 from decimal import Context
 import time
-
+from bs4 import BeautifulSoup
 
 ranks = [10, "Herald 0", 11, "Herald 1", 12, "Herald 2", 13, "Herald 3", 14, "Herald 4", 15, "Herald 5",
          20, "Guardian 0", 21, "Guardian 1", 22, "Guardian 2", 23, "Guardian 3", 24, "Guardian 4", 25, "Guardian 5",
@@ -15,7 +16,7 @@ ranks = [10, "Herald 0", 11, "Herald 1", 12, "Herald 2", 13, "Herald 3", 14, "He
          40, "Archon 0", 41, "Archon 1", 42, "Archon 2", 43, "Archon 3", 44, "Archon 4", 45, "Archon 5",
          50, "Legend 0", 51, "Legend 1", 52, "Legend 2", 53, "Legend 3", 54, "Legend 4", 55, "Legend 5",
          60, "Ancient 0", 61, "Ancient 1", 62, "Ancient 2", 63, "Ancient 3", 64, "Ancient 4", 65, "Ancient 5",
-         70, "Divine 0", 71, "Divine 1", 72, "Divine 2", 73, "Divine 3", 74, "Divine 4", 75, "Divine 5"]
+         70, "Divine 0", 71, "Divine 1", 72, "Divine 2", 73, "Divine 3", 74, "Divine 4", 75, "Divine 5", 76, "Divine 6"]
 
 async def playedWith(author,msg,client,givenID):
     matches = []
@@ -143,8 +144,8 @@ async def queryProfileNoAuth(msg,client,ID):
             losses = str(data2['lose'])
             matches = int(wins) + int(losses)
             ratio = Decimal(int(wins) / matches).quantize(Decimal('.0001'))  # this should be shortened
-            ratio *= 100  # *
-            ratio = str(ratio)  # to one line
+            ratio *= 100                                                     # *
+            ratio = str(ratio)                                               # to one line
             mmr = str(data['competitive_rank'])
             rank = ranks[ranks.index(int(data['rank_tier'])) + 1]
             e = discord.Embed(title=name, url="https://www.dotabuff.com/players/" + ID, color=0xff0000)
@@ -152,15 +153,15 @@ async def queryProfileNoAuth(msg,client,ID):
             e.set_thumbnail(url=thumbnail)
             e.add_field(name="MMR", value=mmr, inline=False)
             if (lBoardRank == "None"):
-                if (ID == "16461605"):
+                if (ID == "16461609"):
                     e.add_field(name="Rank", value="Divine 6 ( 1 )", inline=False)
-                elif (ID == "52771263"):
+                elif (ID == "52771243"):
                     e.add_field(name="Rank", value="Divine 1337 ( ??? )", inline=False)
                 else:
                     e.add_field(name="Rank", value=rank, inline=False)
             else:
                 e.add_field(name="Rank", value="Divine 6  (" + lBoardRank + ")", inline=False)
-            e.add_field(name="Win/Loss", value="Matches: " + wins + "-" + losses + " (" + ratio[:5] + "%)",
+            e.add_field(name="Win/Loss", value= wins + "-" + losses + " (" + ratio[:5] + "%)",
                         inline=False)
             e.add_field(name="Top Heroes", value=heroNamesFinal[0] + " (" + heroWinRates[0][:5] + "%)" +
                                                  "\n" + heroNamesFinal[1] + " (" + heroWinRates[1][:5] + "%)" +
@@ -268,15 +269,15 @@ async def queryProfile(author,msg,client):
             e.set_thumbnail(url=thumbnail)
             e.add_field(name="MMR", value=mmr, inline=False)
             if (lBoardRank == "None"):
-                if (ID == "16461605"):
+                if (ID == "16461602"):              ##if u want to prank someone, add their id here
                     e.add_field(name="Rank", value="Divine 6 ( 1 )", inline=False)
-                elif (ID == "52771263"):
+                elif (ID == "52771363"):
                     e.add_field(name="Rank", value="Divine 1337 ( ??? )", inline=False)
                 else:
                     e.add_field(name="Rank", value=rank, inline=False)
             else:
                 e.add_field(name="Rank", value="Divine 6  (" + lBoardRank + ")", inline=False)
-            e.add_field(name="Win/Loss", value="Matches: " + wins + "-" + losses + " (" + ratio[:5] + "%)",
+            e.add_field(name="Win/Loss", value= wins + "-" + losses + " (" + ratio[:5] + "%)",
                         inline=False)
             e.add_field(name="Top Heroes", value=heroNamesFinal[0] + " (" + heroWinRates[0][:5] + "%)" +
                                                  "\n" + heroNamesFinal[1] + " (" + heroWinRates[1][:5] + "%)" +
@@ -289,6 +290,14 @@ async def queryProfile(author,msg,client):
 
     else:
         await client.send_message(msg.channel, "You have not setup your ID!")
+
+async def checkPricePool():
+    r = requests.get("http://dota2.prizetrac.kr/international2018")
+    soup = BeautifulSoup(r.text, "html.parser")
+    soup2 = BeautifulSoup(r.text, "html.parser")
+    pool = soup.find('h1',{'class':'ui green header'}).text
+    text = soup2.find('div',{'class':'ui green message'}).text
+    return "```TI8 Prize pool atm:\n" + pool + "\n\n" + text + "```"
 
 async def queryLastMatch(author,msg,client):
     print("haloo")
@@ -331,3 +340,117 @@ async def queryLastMatch(author,msg,client):
     except requests.exceptions.RequestException as b:
         print(b)
         await client.send_message(msg.channel, "Invalid ID or fetching failed!")
+
+async def queryHelper(author):
+    playerIDs = []
+    try:
+        whitelist = pickle.load(open("whitelistt.p", "rb"))
+    except (OSError, IOError):
+        whitelist = []
+    try:
+        namesAndIDs = pickle.load(open("save.p", "rb"))
+    except (OSError, IOError):
+        namesAndIDs = []
+    if author in namesAndIDs:
+        index = namesAndIDs.index(author) - 1
+        ID = namesAndIDs[index]
+    try:
+        URL = "https://api.opendota.com/api/players/" + ID + "/recentMatches"
+        r = requests.get(url=URL)
+        data = r.json()
+        matchID = str(data[0]['match_id'])
+        URL2 = "https://api.opendota.com/api/matches/" + matchID
+        r2 = requests.get(url=URL2)
+        data2 = r2.json()
+        if (len(r.content) <= 6):
+            return
+        if (len(r2.content) <= 6):
+            return
+        print("Match ID:  ")
+        print(matchID)
+        i = 0
+        while True:
+            try:
+                add = str(data2['players'][i]['account_id'])
+                if (add != "None"):
+                    if (add not in whitelist):
+                        playerIDs.append(add)
+                i = i + 1
+            except IndexError:
+                break
+        return playerIDs
+    except requests.exceptions.RequestException as b:
+        print(b)
+        return
+
+
+async def playedWithLastGame(givenIDs, myID, msg, client):
+
+    try:
+        whitelist = pickle.load(open("whitelistt.p", "rb"))
+    except (OSError, IOError):
+        whitelist = []
+
+    IDs = []
+    IDs = givenIDs
+
+    print(IDs)
+
+    for x in range(0,len(IDs)):
+        if (givenIDs[x] not in whitelist):
+            matches = []
+            try:
+                URL = "https://api.opendota.com/api/players/" + myID + "/matches?included_account_id=" + givenIDs[x]
+                URL2 = "https://api.opendota.com/api/players/" + givenIDs[x] + "/"
+                r = requests.get(url=URL)
+                r2 = requests.get(url=URL2)
+                data = r.json()
+                data2 = r2.json()
+                if (len(r.content) <= 6):
+                    return
+                if (len(r2.content) <= 6):
+                    return
+                i = 0  #set to 1 if want to skip first game
+                while True:
+                    try:
+                        print(str(data[i]['match_id']))
+                        matches.append(str(data[i]['match_id']))
+                        i = i + 1
+                    except IndexError:
+                        i = 0
+                        break
+                print("Number of matches played with: ")
+                print(len(matches))
+                print("Match id('s): ")
+                e = discord.Embed(title=str(data2['profile']['personaname']),
+                                  url="https://www.dotabuff.com/players/" + givenIDs[x], color=0xffffff)
+                thumbnail = str(data2['profile']['avatarmedium'])
+                e.set_thumbnail(url=thumbnail)
+                for b in range(0, len(matches)):
+                    print(matches[b])
+                    if (len(matches) <= 1):
+                        e.add_field(name='No matches found!',value='^^^^^^^^^^', inline=False)
+                    else:
+                        e.add_field(
+                            name=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(str(data[b]['start_time'])))),
+                            value="https://www.dotabuff.com/matches/" + matches[b], inline=False)
+                await client.send_message(msg.channel, embed=e)
+            except requests.exceptions.RequestException as e:
+                print(e)
+                await client.send_message(msg.channel, "Invalid ID or fetching failed!")
+        else:
+            print(whitelist)
+            print(givenIDs[x])
+async def getProfileID(author):
+    try:
+        namesAndIDs = pickle.load(open("save.p", "rb"))
+    except (OSError, IOError):
+        namesAndIDs = []
+    print(author)
+    if author in namesAndIDs:
+        index = namesAndIDs.index(author) - 1
+        ID = namesAndIDs[index]
+        print("mit vitu")
+        print(ID)
+        return ID
+    return "None"
