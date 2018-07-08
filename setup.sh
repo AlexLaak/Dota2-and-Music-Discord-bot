5c59f212f9f8371ac55a5ec78f850c1ad6e194f0
AlexLaak <alexla@metropolia.fi>
2018-07-08 17:03:03 +0300

    Setup script for Ubuntu

---
+++ setup.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+sudo apt-get install -y git
+sudo apt-get install -y python3
+sudo apt-get install -y python3-pip
+cd Dota2-and-Music-Discord-bot/python
+sudo apt-get install -y ffmpeg
+pip3 install discord.py
+pip3 install beautifulsoup4
+pip3 install PyNaCl
+pip3 install youtube-dl
+echo "----"
+echo "----"
+echo "Remember to update your Discord bot token into Discord.py:1667."
+echo "Make sure you have added bot into your discord server."
+echo "'python3 Discord.py' to run bot"
+read "asd"
